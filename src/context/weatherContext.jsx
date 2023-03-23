25f132018d1a573213135d6b126e5e042215651d
unknown <87931534+HubertK778@users.noreply.github.com>
2023-03-23 19:57:03 +0100

    hour

--- src/context/weatherContext.jsx
+++ src/context/weatherContext.jsx
@@ -1,10 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react'
 
-
-// constans
-const API_KEY = 'b0913a51f81c495b96e200430232103'
-
-
+const API_KEY = import.meta.env.VITE_API_KEY
 const WeatherContext = React.createContext()
 
 export function useWeatherData() {
@@ -12,110 +8,84 @@ export function useWeatherData() {
 }
 
 export default function WeatherProvider({ children }) {
-
-  // weather & sun data
   const [weatherData, setWeatherData] = useState({})
   const [sunData, setSunData] = useState({})
+  const duration = 600000
 
-
-  // fetch weather data q=city
   const getWeatherData = async (q = 'Olesnica') => {
     try {
-        // fetch
-        const response = await fetch(
-          'http://api.weatherapi.com/v1/current.json?' +
-            new URLSearchParams({
-              key: API_KEY,
-              q: q,
-              aqi: 'yes',
-            })
-        )
+      const response = await fetch(
+        'http://api.weatherapi.com/v1/current.json?' +
+          new URLSearchParams({
+            key: API_KEY,
+            q: q,
+            aqi: 'yes',
+          })
+      )
 
-        // response to json
-        const weatherDataJson = await response.json()
-        console.log('Weather data:', weatherDataJson)
+      const weatherDataJson = await response.json()
+      console.log('Weather data:', weatherDataJson)
 
-
-        // save response in state
-        setWeatherData((prev) => weatherDataJson)
-    }
-    catch (error) {
-        console.log(error.message)
+      setWeatherData(weatherDataJson)
+    } catch (error) {
+      console.log(error.message)
     }
   }
 
-
-
-  // fetch sun data q=city
   const getSunData = async (q = 'Olesnica') => {
     try {
-        // fetch
-        const response = await fetch(
-          'http://api.weatherapi.com/v1/astronomy.json?' +
-            new URLSearchParams({
-              key: API_KEY,
-              q: q,
-              aqi: 'yes',
-            })
-        )
+      const response = await fetch(
+        'http://api.weatherapi.com/v1/astronomy.json?' +
+          new URLSearchParams({
+            key: API_KEY,
+            q: q,
+            aqi: 'yes',
+          })
+      )
+      const sunDataJson = await response.json()
+      console.log('Sun data:', sunDataJson)
 
-        // response to json
-        const sunDataJson = await response.json()
-        console.log('Sun data:', sunDataJson)
-
-        // save response in state
-        setSunData((prev) => sunDataJson)
-    } 
-    catch (error) {
-        console.log(error.message)
+      setSunData((prev) => sunDataJson)
+    } catch (error) {
+      console.log(error.message)
     }
   }
 
-
-
-  
   useEffect(() => {
-    //fetch data
     getWeatherData()
     getSunData()
 
-
-    //interval for fetching data
     const interval = setInterval(() => {
       getWeatherData()
       getSunData()
-    }, 60000)
-
+    }, duration)
 
     console.log(weatherData)
-
 
     return () => clearInterval(interval)
   }, [])
 
+  const lastUpdated = weatherData?.current?.last_updated.substring(
+    weatherData?.current?.last_updated.length - 5
+  )
+  //no, I don't know regex 
+  const hourRegex = /^(\d{2}:\d{2})/;
 
+  function getSunset(fullTime) {
+    const hour = parseInt(fullTime.substring(0, 2))
+    const minutes = fullTime.substring(3, 2)
+    console.log(fullTime.substring(0,2))
+    return `${hour + 12}:${minutes}`
+  }
 
-  const lastUpdated = weatherData?.current?.last_updated.substring(weatherData?.current?.last_updated.length - 5)
-
-
-  let sunriseHour = sunData?.astronomy?.astro?.sunset.substring(sunData?.astronomy?.astro?.sunset.length - 6)
-
-
-  sunriseHour = sunriseHour + 12
-
-  console.log(sunriseHour)
-
-
-
-
-  const value =  {
+  const value = {
     temperature: weatherData?.current?.temp_c,
     uv: weatherData?.current?.uv,
     humidity: weatherData?.current?.humidity,
     aq: Math.floor(weatherData?.current?.air_quality.pm10),
-    sunrise: sunData?.astronomy?.astro?.sunrise,
-    sunset: sunData?.astronomy?.astro?.sunset,
-    lastUpdated: lastUpdated
+    sunrise: sunData?.astronomy?.astro?.sunrise.match(hourRegex)[1],
+    // sunset: getSunset(sunData?.astronomy?.astro?.sunset.match(hourRegex)[1]),
+    lastUpdated: lastUpdated,
   }
 
   return (
